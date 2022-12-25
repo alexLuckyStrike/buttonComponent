ad94fca7f8b39e7b387f7299342e34fb973c7e40
Alexbelka <alexbelka1992@mail.ru>
2022-12-25 21:10:26 +0300

    bg

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <test/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import test from "./components/test";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+test
   }
 }
 </script>
---
+++ src/components/test.vue
@@ -0,0 +1,48 @@
+<template>
+<div >
+  <button @click="del">press</button>
+<!--  <ul class="menu" v-for="(item,index) in items" :key="index">-->
+<!--    <li>{{item}}</li>-->
+<!--  </ul>-->
+<!--  <button >alternative</button>-->
+
+  <h3 v-if="items.length >= 2">Vue неплох</h3>
+  <h3 v-else>Полное говно</h3>
+</div>
+</template>
+
+<script>
+export default {
+  name: "test",
+
+  data(){
+    return{
+      items:['first','second']
+    }
+  },
+  methods:{
+    del(){
+      this.items.length
+      this.items.splice(0,this.items.length)
+      console.log(this.items.length)
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+button{
+  display: block;
+  width: 10%;
+  margin:auto;
+}
+
+.menu{
+  display: block;
+  width: 20%;
+  box-shadow: inset 0 0 1px 1px black;
+  margin:auto
+}
+</style>
